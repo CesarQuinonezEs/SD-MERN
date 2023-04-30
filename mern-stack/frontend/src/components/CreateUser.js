1e2cda41d820a2c80bd12f8fdf47f36dd2beb8e2
CesarQuinonezEs <quinonez.cesar@uabc.edu.mx>
2023-04-29 21:24:25 -0700

    build

--- mern-stack/frontend/src/components/CreateUser.js
+++ mern-stack/frontend/src/components/CreateUser.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 const URI = 'https://sdbackend.onrender.com/api/users/';
 
+
 export default class CreateUser extends Component {
     state = {
         users: [],
